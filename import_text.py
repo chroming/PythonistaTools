f833c0d1044b168806598f17c61715dcfd58390b
chroming <guangzhishang@gmail.com>
2017-01-11 13:37:47 +0800

    Fix

--- import_text.py
+++ import_text.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     """
-    iOS中文本文件"分享到其他应用"时自动导入Pythonista的importfile文件夹中
+    iOS中文本文件"分享到其他应用"时自动导入同路径下的importfile文件夹中
     :return: None
     """
     text = appex.get_text()
@@ -13,9 +13,9 @@ def main():
         print 'No input text found. Use this script from the share sheet in an app like Notes.'
         return
     name = os.path.basename(text)
-    with open(text, 'a') as t:
-        t.write(text)
-    print 'SAVE %s.py to importfile OK !' % name
+    with open(text) as t, open('importfile/%s' % name, 'a') as n:
+        n.write(t.read())
+    print 'SAVE %s to importfile OK !' % name
 
 if __name__ == '__main__':
     main()
