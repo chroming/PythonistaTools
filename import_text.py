d582711e2f6e9b2b7b73eaecf7af3cfd079dce2d
chroming <guangzhishang@gmail.com>
2017-01-11 18:28:16 +0800

    分享文本文件到 Pythonista

--- import_text.py
+++ import_text.py
@@ -1,4 +1,11 @@
 # coding: utf-8
+"""
+iOS使用“应用内分享”传递文本时执行此脚本可将文件导入Pythonista 内。
+
+***注意***
+请使用 import_file.py 代替此脚本。此脚本仅可导入文本，import_file 可导入任意格式文件。
+
+"""
 import appex
 import os
 
@@ -13,7 +20,7 @@ def main():
         print 'No input text found. Use this script from the share sheet in an app like Notes.'
         return
     name = os.path.basename(text)
-    with open(text) as t, open('importfile/%s' % name, 'a') as n:
+    with open(text) as t, open(name, 'w+') as n:
         n.write(t.read())
     print 'SAVE %s to importfile OK !' % name
 
